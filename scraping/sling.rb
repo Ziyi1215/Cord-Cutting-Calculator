cc9631cfdfc8c7b60959a37cc1c336bec8e6036f
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2019-10-30 15:41:34 +0000

    Add ons: Only channels

---
+++ scraping/sling.rb
@@ -0,0 +1,24 @@
+require 'open-uri'
+require 'nokogiri'
+require 'json'
+require 'httparty'
+
+class Scraper
+    attr_accessor :parse_page
+    def initialize
+        doc = HTTParty.get('https://www.sling.com')
+        @parse_page ||= Nokogiri::HTML(doc)
+    end
+end
+
+scraper = Scraper.new()
+
+scraper.parse_page.css('//div[@data-sling-tab-name]').each do |category|
+    addon_category = category['data-sling-tab-name']
+    puts "***************"
+    puts addon_category
+    puts "***************"
+    category.css('.carousel-jam_channel-container').css('img').each do |image|
+        puts image['alt']
+    end
+end
