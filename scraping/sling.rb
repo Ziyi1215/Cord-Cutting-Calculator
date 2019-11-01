b9d783aa74b5de26495152a7a2f740468de5ad82
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2019-11-01 22:35:43 +0000

    Addons + Channel + Price information done

--- scraping/sling.rb
+++ scraping/sling.rb
@@ -5,13 +5,16 @@ require 'httparty'
 
 class Scraper
     attr_accessor :parse_page
-    def initialize
-        doc = HTTParty.get('https://www.sling.com')
+    def get_url(url)
+        HTTParty.get(url)
+    end
+    def parse_url(doc)
         @parse_page ||= Nokogiri::HTML(doc)
     end
 end
 
 scraper = Scraper.new()
+scraper.parse_url(scraper.get_url('https://www.sling.com'))
 
 scraper.parse_page.css('//div[@data-sling-tab-name]').each do |category|
     addon_category = category['data-sling-tab-name']
@@ -23,4 +26,29 @@ scraper.parse_page.css('//div[@data-sling-tab-name]').each do |category|
     end
     cost = category.css('.carousel-jam_heading').text[/[\d]+/]
     puts cost
+end
+
+file_list = ['orange.htm', 'blue.htm', 'orange_blue.htm']
+
+file_list.each do |file|
+    puts "***************"
+    puts file
+    puts "***************"
+    
+    all_channels_scraper = Scraper.new()
+    all_channels_scraper.parse_url(File.read(file))
+    
+    cost_list = all_channels_scraper.parse_page.css('.dyn-grid_package-title')
+    name_list = all_channels_scraper.parse_page.css('.dyn-grid_package-subtitle')
+    
+    cost_list.zip(name_list).each do |cost, package|
+        puts package.text, cost.text[/[\d]+/]
+    end
+    puts
+    
+    puts all_channels_scraper.parse_page.css('#channelList').css('img').count
+    
+    all_channels_scraper.parse_page.css('#channelList').css('img').each do |image|
+        puts image['alt']
+    end 
 end
