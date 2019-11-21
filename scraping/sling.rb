ed9a8344f2c0a07de46b894be3683538ef8e830a
EC2 Default User <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2019-11-21 23:03:11 +0000

    Sling Scraping Done

--- scraping/sling.rb
+++ scraping/sling.rb
@@ -1,55 +1,46 @@
-require 'open-uri'
+require 'watir'
 require 'nokogiri'
-require 'json'
-require 'httparty'
-
-class Scraper
-    attr_accessor :parse_page
-    def get_url(url)
-        HTTParty.get(url)
-    end
-    def parse_url(doc)
-        @parse_page ||= Nokogiri::HTML(doc)
-    end
-end
 
 
 def get_packages
-    scraper = Scraper.new()
-    scraper.parse_url(scraper.get_url('https://www.sling.com'))
-    file_list = ['orange.htm', 'blue.htm', 'orange_blue.htm']
     index = 0
     packages_list = []
+    browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
     
-    file_list.each do |file|
-        all_channels_scraper = Scraper.new()
-        all_channels_scraper.parse_url(File.read(file))
+    ["planOne", "planTwo", "planThree"].each do |package|
+        browser.goto('https://www.sling.com/')
+        browser.a(:id => package).fire_event :click
+        html_doc = Nokogiri::HTML(browser.html)
         
-        cost_list = all_channels_scraper.parse_page.css('.dyn-grid_package-title')
-        name_list = all_channels_scraper.parse_page.css('.dyn-grid_package-subtitle')
+        cost_list = html_doc.css('.dyn-grid_package-title')
+        name_list = html_doc.css('.dyn-grid_package-subtitle')
         
         package_cost_list = cost_list.zip(name_list).map do |cost, package|
             [package.text, cost.text[/[\d]+/]]
         end
         
-        count = all_channels_scraper.parse_page.css('#channelList').css('img').count
-        channels_list = all_channels_scraper.parse_page.css('#channelList').css('img').map do |image|
-            image['title']
-        end 
+        count = html_doc.css('#channelList').css('img').count
+        channels_list = html_doc.css('#channelList').css('img').map do |channel|
+            channel['title']
+        end
         
         packages_list << [package_cost_list[index][0], channels_list, count, package_cost_list[index][1]]
         index += 1
     end
+    
     packages_list
 end
 
 # puts get_packages
 
+
 def get_addons
-    scraper = Scraper.new()
-    scraper.parse_url(scraper.get_url('https://www.sling.com'))
+    browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
+    browser.goto('https://www.sling.com/')
+    html_doc = Nokogiri::HTML(browser.html)
+    
     addon_list = []
-    scraper.parse_page.css('//div[@data-sling-tab-name]').each do |category|
+    html_doc.css('//div[@data-sling-tab-name]').each do |category|
         addon_category = category['data-sling-tab-name']
         channels_list = category.css('.carousel-jam_channel-container').css('img').map do |image|
             image['alt']
@@ -57,17 +48,8 @@ def get_addons
         cost = category.css('.carousel-jam_heading').text[/[\d]+/]
         addon_list << [addon_category, channels_list, cost]
     end
+    
     addon_list
 end
 
-
-# require 'watir'
-# require 'nokogiri'
-# browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
-# browser.goto('https://www.sling.com/')
-# html_doc = Nokogiri::HTML(browser.html)
-# channel_list = html_doc.css('li')
-# puts channel_list.count
-# channel_list.each do |image|
-#     puts image['title']
-# end
+# puts get_addons
