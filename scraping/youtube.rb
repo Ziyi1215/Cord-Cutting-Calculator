c90bc769a7204be5c4ac688834aa5086bbc99efa
sbuvaneish <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2019-12-01 21:42:18 +0000

    youtube scraping done without zipcode

--- scraping/youtube.rb
+++ scraping/youtube.rb
@@ -1,10 +1,28 @@
 require 'watir'
 require 'nokogiri'
 
-browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
-browser.goto('https://tv.youtube.com/welcome')
-browser.element(tag_name: "article", class: "ytv-surface__content").wait_until(&:present?).div(class: ["js-input", "input"]).wait_until(&:present?)
-browser.text_field(aria_label: "Enter your home ZIP code").set('77840')
-browser.button(class: ["js-input-button", "input__submit-button", "ytv-button"]).click
-html_doc = Nokogiri::HTML(browser.html)
-puts html_doc
+# browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
+# browser.goto('https://tv.youtube.com/welcome')
+# browser.element(tag_name: "article", class: "ytv-surface__content").wait_until(&:present?).div(class: ["js-input", "input"]).wait_until(&:present?)
+# browser.text_field(aria_label: "Enter your home ZIP code").set('77840')
+# browser.button(class: ["js-input-button", "input__submit-button", "ytv-button"]).click
+# html_doc = Nokogiri::HTML(browser.html)
+# puts html_doc
+
+def get_packages
+    browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => '/usr/bin/google-chrome'}})
+    browser.goto('https://tv.youtube.com/welcome')
+    browser.a(class: ["ytv-link", "js-click"]).click
+    html_doc = Nokogiri::HTML(browser.html)
+    
+    channels_list = html_doc.css('div.zip__network').map do |channel|
+        channel.css('p').text
+    end
+    
+    price = html_doc.css('span.price').text[/[\d]+[,.][\d]+/]
+    
+    ["Youtube TV", channels_list, channels_list.count, price]
+end
+
+puts get_packages
+
