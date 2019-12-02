8824e8227a7cce584433202a0f38ca80aa4092d5
EC2 Default User <ec2-user@ip-172-31-39-171.us-east-2.compute.internal>
2019-12-02 15:06:25 +0000

    adding changes for hulu

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -85,6 +85,34 @@ def get_packages_local_youtube
     [["Youtube TV", channels_list, channels_list.count, price]]
 end
 
+def get_packages_local_hulu
+	html_doc = Nokogiri::HTML(File.read("scraping/live-tv"))
+	channels_list = html_doc.css('img').map do |image|
+							class_icon = image['class']
+							network_icon = 'network-icon ls-is-cached lazyloaded'
+							network_icon1 = 'network-icon lazyload'
+							if class_icon.to_s == network_icon.to_s || class_icon.to_s == network_icon1.to_s
+									image['alt']
+							end
+			end
+
+	req_icon_pricing =  'jsx-3636485725 plan-card__priceline'
+	price = html_doc.css("div").map do |divisions|
+					class_icon = divisions['class']
+					if class_icon.to_s == req_icon_pricing.to_s
+							divisions.css('p').map do |addons|
+									class_icon = addons['class']
+									req_icon_price = 'jsx-3636485725 plan-card__amount'
+									if class_icon.to_s == req_icon_price.to_s
+													addons.text
+									end
+							end
+					end
+		end
+	channels_list.delete(nil)
+	price.delete(nil)
+	[["Hulu Live TV", channels_list, channels_list.count, price[0]]]
+end
 
 class ScrapersController < ApplicationController
     def select_package
@@ -98,6 +126,8 @@ class ScrapersController < ApplicationController
             @packages = get_packages_local_att
         elsif params[:service] == "youtube" 
             @packages = get_packages_local_youtube
+        elsif params[:service] == "hulu"
+	    @packages = get_packages_local_hulu
         end
     end
 end
