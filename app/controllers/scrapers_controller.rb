28a65a12087ccac8ebb6e06b90e3be4838d33f6c
sbuvaneish <ec2-user@ip-172-31-46-247.us-east-2.compute.internal>
2019-12-05 22:54:05 +0000

    Getting sling to work on remote

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -28,9 +28,63 @@ def get_packages_local_sling
     packages_list
 end
 
+def get_packages_sling
+    index = 0
+    packages_list = []
+    chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+    browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => chrome_bin}})
+    
+    ["planOne", "planTwo", "planThree"].each do |package|
+        browser.goto('https://www.sling.com/')
+        browser.a(:id => package).fire_event :click
+        html_doc = Nokogiri::HTML(browser.html)
+        
+        cost_list = html_doc.css('.dyn-grid_package-title')
+        name_list = html_doc.css('.dyn-grid_package-subtitle')
+        
+        package_cost_list = cost_list.zip(name_list).map do |cost, pack|
+            [pack.text, cost.text[/[\d]+/]]
+        end
+        
+        count = html_doc.css('#channelList').css('img').count
+        channels_list = html_doc.css('#channelList').css('img').map do |channel|
+            channel['title']
+        end
+        
+        packages_list << [package_cost_list[index][0], channels_list, count, package_cost_list[index][1]]
+        index += 1
+    end
+    
+    browser.close
+    
+    packages_list
+end
+
 
 def get_addons_local_sling
     html_doc = Nokogiri::HTML(File.read("scraping/orange.html"))
+    
+    addon_list = []
+    html_doc.css('//div[@data-sling-tab-name]').each do |category|
+        addon_category = category['data-sling-tab-name']
+        channels_list = category.css('.carousel-jam_channel-container').css('img').map do |image|
+            image['alt']
+        end
+        cost = category.css('.carousel-jam_heading').text[/[\d]+/]
+        count = channels_list.count
+        addon_list << [addon_category, channels_list, count, cost]
+    end
+    
+    addon_list
+end
+
+
+def get_addons_sling
+    chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+    browser = Watir::Browser.new(:chrome, {:chromeOptions => {:args => ['--headless', '--window-size=1200x600'], :binary => chrome_bin}})
+    browser.goto('https://www.sling.com/')
+    html_doc = Nokogiri::HTML(browser.html)
+    browser.close
     
     addon_list = []
     html_doc.css('//div[@data-sling-tab-name]').each do |category|
@@ -168,8 +222,8 @@ class ScrapersController < ApplicationController
     
     def show_package
         if params[:service] == "sling"
-            @addons = get_addons_local_sling
-            @packages = get_packages_local_sling
+            @addons = get_addons_sling
+            @packages = get_packages_sling
         elsif params[:service] == "att"
             @packages = get_packages_att
         elsif params[:service] == "youtube" 
